f81ce2509366ff3620ac8f299e0e7c4706e1dca9
snoupyrelete <drob8896@740s-pl-112.local>
2017-05-04 09:39:33 -0600

    Initial commit

---
+++ GameProject/Controller/ShooterGame.cs
@@ -0,0 +1,155 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+using GameProject.Model;
+
+namespace GameProject
+{
+	/// <summary>
+	/// This is the main type for your game.
+	/// </summary>
+	public class ShooterGame : Game
+	{
+		#region Declaration Section
+		private GraphicsDeviceManager graphics;
+		private SpriteBatch spriteBatch;
+		private Player player;
+
+		// Keyboard states used to determine key presses
+		private KeyboardState currentKeyboardState;
+		private KeyboardState previousKeyboardState;
+
+		// Gamepad states used to determine button presses
+		private GamePadState currentGamePadState;
+		private GamePadState previousGamePadState;
+
+		// A movement speed for the player
+		private float playerMoveSpeed;
+		#endregion
+
+		public ShooterGame()
+		{
+			graphics = new GraphicsDeviceManager(this);
+			Content.RootDirectory = "Content";
+		}
+
+		/// <summary>
+		/// Allows the game to perform any initialization it needs to before starting to run.
+		/// This is where it can query for any required services and load any non-graphic
+		/// related content.  Calling base.Initialize will enumerate through any components
+		/// and initialize them as well.
+		/// </summary>
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+			player = new Player();
+
+			// Set a constant player move speed
+			playerMoveSpeed = 8.0f;
+
+
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// LoadContent will be called once per game and is the place to load
+		/// all of your content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			// Create a new SpriteBatch, which can be used to draw textures.
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+
+			//TODO: use this.Content to load your game content here 
+			//Load the player resources 
+			Vector2 playerPosition = new Vector2(GraphicsDevice.Viewport.TitleSafeArea.X, GraphicsDevice.Viewport.TitleSafeArea.Y + GraphicsDevice.Viewport.TitleSafeArea.Height / 2);
+			player.Initialize(Content.Load<Texture2D>("Texture/player"), playerPosition);
+		}
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update(GameTime gameTime)
+		{
+			// For Mobile devices, this logic will close the Game when the Back button is pressed
+			// Exit() is obsolete on iOS
+#if !__IOS__ && !__TVOS__
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+				Exit();
+#endif
+
+			// TODO: Add your update logic here
+
+			// Save the previous state of the keyboard and game pad so we can determinesingle key/button presses
+			previousGamePadState = currentGamePadState;
+			previousKeyboardState = currentKeyboardState;
+
+			// Read the current state of the keyboard and gamepad and store it
+			currentKeyboardState = Keyboard.GetState();
+			currentGamePadState = GamePad.GetState(PlayerIndex.One);
+
+
+			//Update the player
+			UpdatePlayer(gameTime);
+
+			base.Update(gameTime);
+		}
+
+		private void UpdatePlayer(GameTime gameTime)
+		{
+
+			// Get Thumbstick Controls
+			player.Position.X += currentGamePadState.ThumbSticks.Left.X * playerMoveSpeed;
+			player.Position.Y -= currentGamePadState.ThumbSticks.Left.Y * playerMoveSpeed;
+
+			// Use the Keyboard / Dpad
+			if (currentKeyboardState.IsKeyDown(Keys.A) ||
+			currentGamePadState.DPad.Left == ButtonState.Pressed)
+			{
+				player.Position.X -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.D) ||
+			currentGamePadState.DPad.Right == ButtonState.Pressed)
+			{
+				player.Position.X += playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.W) ||
+			currentGamePadState.DPad.Up == ButtonState.Pressed)
+			{
+				player.Position.Y -= playerMoveSpeed;
+			}
+			if (currentKeyboardState.IsKeyDown(Keys.S) ||
+			currentGamePadState.DPad.Down == ButtonState.Pressed)
+			{
+				player.Position.Y += playerMoveSpeed;
+			}
+
+			// Make sure that the player does not go out of bounds
+			player.Position.X = MathHelper.Clamp(player.Position.X, 0, GraphicsDevice.Viewport.Width - player.Width);
+			player.Position.Y = MathHelper.Clamp(player.Position.Y, 0, GraphicsDevice.Viewport.Height - player.Height);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw(GameTime gameTime)
+		{
+			graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+			//TODO: Add your drawing code here
+			spriteBatch.Begin();
+
+			player.Draw(spriteBatch);
+
+			spriteBatch.End();
+
+			base.Draw(gameTime);
+		}
+	}
+}
---
+++ GameProject/Model/Player.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameProject.Model
+{
+	public class Player
+	{
+		// Animation representing the player
+		private Texture2D playerTexture;
+		public Texture2D PlayerTexture
+		{
+			get { return playerTexture; }
+			set { playerTexture = value; }
+		}
+
+		// Position of the Player relative to the upper left side of the screen
+		public Vector2 Position;
+
+		// State of the player
+		private bool active;
+		public bool Active 
+		{
+			get { return active; }
+			set { active = value; } 
+		}
+
+		// Amount of hit points that player has
+		private int health;
+		public int Health 
+		{
+			get { return health; }
+			set { health = value;}
+		}
+
+		// Get the width of the player ship
+		private int width;
+		public int Width
+		{
+			get { return PlayerTexture.Width; }
+		}
+
+		// Get the height of the player ship
+		private int height;
+		public int Height
+		{
+			get { return PlayerTexture.Height; }
+		}
+
+		public Player()
+		{
+		}
+
+		public void Initialize(Texture2D texture, Vector2 position)
+		{
+			PlayerTexture = texture;
+
+			// Set the starting position of the player around the middle of the screen and to the back
+			Position = position;
+
+			// Set the player to be active
+			Active = true;
+
+			// Set the player health
+			Health = 100;
+		}
+
+		public void Update()
+		{
+		}
+
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			spriteBatch.Draw(PlayerTexture, Position, null, Color.White, 0f, Vector2.Zero, 1f, SpriteEffects.None, 0f);
+		}
+	}
+}
